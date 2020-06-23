25ff06b6d63f4db937bd0fa5f6190785e3836a65
banjun <banjun@gmail.com>
2020-06-24 08:33:51 +0900

    Initial Commit

---
+++ Xcode114Linking/Xcode114Linking.h
@@ -0,0 +1,12 @@
+//
+//  Xcode114Linking.h
+//  Xcode114Linking
+//
+//  Created by jun.ban on R 2/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Xcode114Linking : NSObject
+
+@end
---
+++ Xcode114Linking/Xcode114Linking.m
@@ -0,0 +1,12 @@
+//
+//  Xcode114Linking.m
+//  Xcode114Linking
+//
+//  Created by jun.ban on R 2/06/24.
+//
+
+#import "Xcode114Linking.h"
+
+@implementation Xcode114Linking
+
+@end
